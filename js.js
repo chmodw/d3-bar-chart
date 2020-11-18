42f40b892bbc44ed6f6499544a3086de013f424d
chamodya wimansha <chamodyawimansha@gmail.com>
2020-11-18 17:36:38 +0530

    mouse over effect added

--- js.js
+++ js.js
@@ -10,8 +10,6 @@ var chartContainer = d3
   .attr("height", height + 100)
   .style("padding", 20);
 
-// const barWidth = width / data.count()
-
 d3.json(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 )
@@ -42,12 +40,6 @@ d3.json(
       .domain([0, d3.max(data.map((item) => item.gdp))])
       .range([0, height]);
 
-    let scaledGDP = data.map((item) => linearScale(item.gdp));
-
-    // data.forEach((i) => {
-    //   data[i].barHeight = linearScale(item.gdp);
-    // });
-
     chartContainer
       .append("g")
       .call(xAxis)
@@ -60,31 +52,37 @@ d3.json(
       .attr("id", "y-axis")
       .attr("transform", "translate(60, 0)");
 
+    /**
+     * Adding bars to the chart
+     */
     d3.select("svg")
       .selectAll("rect")
-      .data(scaledGDP)
+      .data(data)
       .enter()
       .append("rect")
-      .attr("data-date", (d, i) => data[i].date)
-      .attr("data-gdp", (d, i) => data[i].gdp)
       .attr("class", "bar")
-      .attr("y", (d) => height - d)
-      .attr("x", (d, i) => xScale(data[i].date))
-      .attr("width", width / 275)
-      .attr("height", (d) => d)
+      .attr("id", (d) => "bar-" + d.year + "-" + d.quarter)
+      .attr("y", (d) => height - linearScale(d.gdp))
+      .attr("x", (d) => xScale(d.date))
+      .attr("width", width / data.length)
+      .attr("height", (d) => linearScale(d.gdp))
+      .attr("data-date", (d) => d.date)
+      .attr("data-gdp", (d) => d.gdp)
       .attr("transform", "translate(60, 0)")
-      .style("fill", "#33adff");
+      .style("fill", "rgb(2, 70, 2)")
+      .on("mouseover", (m, d) => {
+        d3.select("#bar-" + d.year + "-" + d.quarter).style("fill", "#FFF");
+      })
+      .on("mouseout", (m, d) => {
+        d3.select("#bar-" + d.year + "-" + d.quarter).style("fill", "#024602");
+      });
   })
   .catch((error) => {
     if (error) throw error;
   });
 
-/**
- * @param {object} data
- * @return {year : "", gdp : "", quarter : ""}
- */
 function modifier(data) {
-  let modified = data.map((item) => {
+  return data.map((item) => {
     return {
       date: new Date(item[0]),
       year: parseInt(item[0].substring(0, 4)),
@@ -93,8 +91,6 @@ function modifier(data) {
       barHeight: 0,
     };
   });
-
-  return modified;
 }
 
 function getQuarters(date) {
@@ -108,72 +104,3 @@ function getQuarters(date) {
     return "Q4";
   }
 }
-
-// /**
-//  * add x and y scales
-//  */
-// let xScale = d3
-//   .scaleTime()
-//   .domain([
-//     d3.min(data.map((item) => item.date)),
-//     d3.max(data.map((item) => item.date)),
-//   ])
-//   .range([0, width]);
-
-// let yScale = d3
-//   .scaleLinear()
-//   .domain([0, d3.max(data.map((item) => item.gdp))])
-//   .range([height, 0]);
-
-// let xAxis = d3.axisBottom().scale(xScale);
-// var yAxis = d3.axisLeft(yScale);
-
-// chartContainer
-//   .append("g")
-//   .call(xAxis)
-//   .attr("id", "x-axis")
-//   .attr("transform", "translate(60, 400)");
-
-// chartContainer
-//   .append("g")
-//   .call(yAxis)
-//   .attr("id", "y-axis")
-//   .attr("transform", "translate(60, 0)");
-
-// /**
-//  *
-//  * Change this later
-//  *
-//  */
-// var linearScale = d3
-//   .scaleLinear()
-//   .domain([0, d3.max(data.map((item) => item.gdp))])
-//   .range([0, height]);
-
-// /**
-//  * add chart bars
-//  */
-// d3.select("svg")
-//   .selectAll("rect")
-//   .data(
-//     data
-//       .map((d) => d.gdp)
-//       .map((d) => {
-//         linearScale(d);
-//       })
-//   )
-//   .enter()
-//   .append("rect")
-//   .attr("class", "bar")
-//   .attr("data-date", data.date)
-//   .attr("data-gdp", data.gdp)
-//   .attr("x", 0)
-//   .attr("y", function (d) {
-//     return height - d;
-//   })
-//   .attr("width", width / 275)
-//   .attr("height", function (d) {
-//     return d;
-//   })
-//   .style("fill", "#33adff")
-//   .attr("transform", "translate(60, 0)");
